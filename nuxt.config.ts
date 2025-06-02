5f4ee5001b6c3ab432bc1f79654bd41e21836fe9
Dario Ureña <darioaxel@gmail.com>
2025-06-02 19:28:53 +0200

    añadido shadcn y creado un boton. Funciona!

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
@@ -5,12 +6,22 @@ export default defineNuxtConfig({
   content: {
     experimental: { nativeSqlite: true },
   },
-
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    prefix: '', // Prefijo opcional para los componentes
+    componentDir: './components/ui' // Directorio de componentes
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
-    '@nuxt/image'
+    '@nuxt/image',
+    'shadcn-nuxt'
   ]
 })
