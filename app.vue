2ab84d3be1670de23a3f720da39ec7f5ca2c9a71
Dario Ureña <darioaxel@gmail.com>
2025-06-12 17:14:11 +0200

    usando layouts

--- app.vue
+++ app.vue
@@ -1,6 +1,23 @@
+<script setup lang="ts">
+// import { Toaster } from '@/registry/new-york-v4/ui/sonner'
+
+const activeTheme = useCookie<string>('active_theme', { readonly: true })
+const isScaled = computed(() => !!activeTheme.value?.endsWith('-scaled'))
+const colorMode = useColorMode()
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-     <Button>Click me</Button>
-  </div>
+  <Body
+    class="bg-background overscroll-none font-sans antialiased"
+    :class="[
+      activeTheme ? `theme-${activeTheme}` : '',
+      isScaled ? 'theme-scaled' : '',
+    ]"
+  >
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+
+    <!-- Toaster :theme="colorMode.preference as any || 'system'" / -->
+  </Body>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,7 @@ export default defineNuxtConfig({
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
-    'shadcn-nuxt'
+    'shadcn-nuxt',
+    '@nuxtjs/color-mode'
   ]
 })
