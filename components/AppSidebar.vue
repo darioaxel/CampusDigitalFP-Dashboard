cf7621c4d15b4ce71441a8c06d7410bf32f31916
Dario Ureña <darioaxel@gmail.com>
2025-06-17 16:12:35 +0200

    a sidebar with icons working! incredible!

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -1,20 +1,65 @@
 <script setup lang="ts">
+import { LucideCalendar, LucideHome, LucideInbox, LucideSearch, LucideSettings } from "#components";
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
-  SidebarHeader,
-} from '@/components/ui/sidebar'
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: LucideHome,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: LucideInbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: LucideCalendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: LucideSearch,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: LucideSettings,
+  },
+];
+
 </script>
 
 <template>
   <Sidebar>
-    <SidebarHeader />
     <SidebarContent>
-      <SidebarGroup />
-      <SidebarGroup />
+      <SidebarGroup>
+        <SidebarGroupLabel>Application</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+              <SidebarMenuItem v-for="item in items" :key="item.title">
+                <SidebarMenuButton asChild>
+                    <a :href="item.url">
+                      <component :is="item.icon" />
+                      <span>{{item.title}}</span>
+                    </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
     </SidebarContent>
-    <SidebarFooter />
   </Sidebar>
 </template>
